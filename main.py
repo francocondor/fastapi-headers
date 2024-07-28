436ad989afbc86236ef6f9ae05d1034e5d638b15
francobko <franco.condor@urp.edu.pe>
2024-07-27 20:50:14 -0500

    feat: add request object to dashboard endpoint

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from typing import Annotated
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, Request
 
 app = FastAPI()
 
 @app.get("/dashboard")
 def dashboard(
+    request: Request,
     access_token: Annotated[str | None, Header()] = None,
     user_role: Annotated[str | None, Header()] = None,
-
     ):
+    print(request.headers)
     return {"access_token:": access_token, "user_role:": user_role}
