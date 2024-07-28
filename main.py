2dd796ab2a7ebf99b369df809c87c3070a7a3350
francobko <franco.condor@urp.edu.pe>
2024-07-27 20:49:10 -0500

    feat: add user role header to dashboard endpoint

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ from fastapi import FastAPI, Header
 app = FastAPI()
 
 @app.get("/dashboard")
-def dashboard(access_token: Annotated[str | None, Header()] = None):
-    return "dashboard"
+def dashboard(
+    access_token: Annotated[str | None, Header()] = None,
+    user_role: Annotated[str | None, Header()] = None,
+
+    ):
+    return {"access_token:": access_token, "user_role:": user_role}
