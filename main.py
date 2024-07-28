211a606fab81304811e4d3302365bfdde9a09edc
francobko <franco.condor@urp.edu.pe>
2024-07-27 20:51:26 -0500

    feat: add response object to dashboard endpoint

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from typing import Annotated
-from fastapi import FastAPI, Header, Request
+from fastapi import FastAPI, Header, Request, Response
 
 app = FastAPI()
 
 @app.get("/dashboard")
 def dashboard(
     request: Request,
+    response: Response,
     access_token: Annotated[str | None, Header()] = None,
     user_role: Annotated[str | None, Header()] = None,
     ):
-    print(request.headers)
+    response.headers["user_status"] = "enabled"
     return {"access_token:": access_token, "user_role:": user_role}
