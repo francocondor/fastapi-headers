0ffb5a2c97f7fd89c01b82ba32d4f10a9e20880d
francobko <franco.condor@urp.edu.pe>
2024-08-06 15:26:12 -0500

    Refactorizar endpoint de dashboard para aceptar un token de acceso opcional

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from fastapi import FastAPI, HTTPException, Header, Request, Response, Depends
 
 app = FastAPI()
 
-def get_headers(        
+
+def get_headers(
     access_token: Annotated[str | None, Header()] = None,
     user_role: Annotated[list[str] | None, Header()] = None,
 ):
     if access_token != "secret-token":
         raise HTTPException(status_code=400, detail="No autorizado")
     return {"access_token": access_token, "user_role": user_role}
+
 
 @app.get("/dashboard")
 def dashboard(headers: Annotated[dict, Depends(get_headers)]):
